efaadd58148034061396589c2cbbbddb2b4b20cc
Juan Gonzalez <3232rose@gmail.com>
2019-08-10 19:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,27 @@ return string.toLowerCase();
 }
 
 function logShout(string) {
- console.log(string.toUpperCase()) 
+ console.log(string.toUpperCase()) ;
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  (string.toLowerCase()===string) 
+  if (string.toLowerCase()===string) ;
   return"I can't hear you!"
 }
 
+if (string.toUpperCase()===string) {
+  return "YES INDEED!"
+}
+
+if (string==="I love you, Grandma.") {
+  return"I love you, too."
+}
+
+}
+
+
+}
